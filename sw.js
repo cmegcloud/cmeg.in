37ce3dbada49f7c890667fb8465f0d95ea7157c2
cmegcloud <cmexpertsgroup@gmail.com>
2026-03-07 03:02:41 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "cmeg-app-v1";
+
+const urlsToCache = [
+
+"/",
+"/manifest.json",
+
+"/admin/app/",
+"/admin/app/index.html",
+
+"/htmlviewer/",
+"/calendar/"
+
+];
+
+self.addEventListener("install", event => {
+
+event.waitUntil(
+
+caches.open(CACHE_NAME).then(cache => {
+return cache.addAll(urlsToCache);
+})
+
+);
+
+});
+
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+
+caches.match(event.request).then(response => {
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
