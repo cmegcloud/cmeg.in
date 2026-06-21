94e40c20c0b966224f124f2dd9bf20b9d7498b5a
cmeg <cmexpertsgroup@gmail.com>
2026-06-21 20:01:57 +0530

    Update sw.js

--- bizze/sw.js
+++ bizze/sw.js
@@ -1,22 +1,24 @@
-const CACHE_NAME = "cm-bizo-v1";
+const CACHE_NAME = 'bizze-v1';
 const urlsToCache = [
-  "./index.html",
-  "./manifest.json",
-  "./assets/logo.png"
+  './index.html',
+  './manifest.json',
+  './assets/logo.png',
+  './assets/name.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request);
+      })
   );
 });
